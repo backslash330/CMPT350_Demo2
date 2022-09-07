dcfea7ee9a2d6eb5f79cf3ef15e739eb5bb51311
Nicholas Almeida <n.almeida330@gmail.com>
2022-09-07 17:49:56 -0600

    added greeting and input collection

--- demo2.s
+++ demo2.s
@@ -13,8 +13,14 @@
 ################################# 
 
 #The data section reserves memory locations for future reference in the program. Commonly used for strings and arrays
-
 .data
+	user_input: .space 64
+	instructions: .asciiz "Please enter your name: "
+	greeting1: .asciiz "Hello "
+	greeting2: .asciiz ", I will modify your name."
+	upper_label: .asciiz "Your name in all uppercase is: "
+	lower_label: .asciiz "Your name in all lowercase is: "
+	nl: .asciiz "\n"
 
 
 #The text section contains the instructions to execute. Most of your time will be spent in the .text section. The text section must have a main: label to function properly
@@ -22,6 +28,37 @@
 
 #the main: label tells SPIM where to begin progam execution
 main:
+	# print instructions
+	li $v0, 4
+	la $a0, instructions
+	syscall
+
+	# print newline
+	la $a0, nl
+	syscall
+
+	# read user input into user_input
+	li $v0, 8
+	la $a0, user_input
+	li $a1, 65
+	syscall
+
+	# print newline
+	li $v0, 4
+	la $a0, nl
+	syscall
+
+	# print greeting1
+	la $a0, greeting1
+	syscall
+
+	# print user_input
+	la $a0, user_input
+	syscall
+
+	# print greeting2
+	la $a0, greeting2
+	syscall
 
 	li $v0, 10 #exit the program 
 	syscall
